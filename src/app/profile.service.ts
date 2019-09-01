761f8097b9ddfcfa799b449d3ed85909f9a2eb14
ClarisseU <clarisseum@gmail.com>
2019-09-01 18:54:55 +0200

    add clientid

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+private username:string;
+private clientid ='94fac37d9c2667c93724';
+private clientsercret = 'c00c3a6fae6672a448f04a1261a4cc8be6eae372';
+
+  constructor(private http:HttpClient) { }
 }
