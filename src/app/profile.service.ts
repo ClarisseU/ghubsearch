b7ee3f8c772b48cf7c883057b66c799e175fdad6
ClarisseU <clarisseum@gmail.com>
2019-09-01 19:05:01 +0200

    add the url

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -8,7 +8,13 @@ export class ProfileService {
 
 private username:string;
 private clientid ='94fac37d9c2667c93724';
-private clientsercret = 'c00c3a6fae6672a448f04a1261a4cc8be6eae372';
+private clientsecret = 'c00c3a6fae6672a448f04a1261a4cc8be6eae372';
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) { 
+    console.log("service ready");
+    this.username = 'ClarisseU';
+  }
+  getInformation(){
+    return this.http.get('https://api.github.com/users/'+ this.username +'/client_id='+this.clientid + '&client_secret=' +this.clientsecret);
+  }
 }
